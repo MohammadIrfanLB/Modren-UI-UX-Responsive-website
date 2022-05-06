ad01b330612b28e3c37b85563fdb995770cf9ac5
Irfan <mohammedirf500@gmail.com>
2022-05-06 19:28:08 +0530

    first commit

---
+++ src/components/brand/import.js
@@ -0,0 +1,7 @@
+import google from "../../Images/google.png";
+import slack from "../../Images/slack.png";
+import atlassian from "../../Images/atlassian.png";
+import dropbox from "../../Images/dropbox.png";
+import shopify from "../../Images/shopify.png";
+
+export { google, slack, atlassian, dropbox, shopify };
